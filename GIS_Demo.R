2d75da8d625871a90e8c6a55ccf10e82b281d899
Shreena Pyakurel <54719859+spshreena@users.noreply.github.com>
2020-11-16 15:14:37 -0500

    Create GIS_Demo.R

---
+++ GIS_Demo.R
@@ -0,0 +1,42 @@
+---
+title: "R Notebook"
+authors: Miles Van Denburg and Shreena Pyakurel
+output: html_notebook
+---
+
+Initiate Google Earth engine and load libraries  
+```{r}
+remotes::install_github("r-spatial/rgee", force = TRUE)
+library(rgee)
+library(sf)
+library(tidyverse)
+```
+
+Install ee_install to get earth engine function 
+
+```{r}
+ee_install() #only need to run once
+ee_Initialize(email = 'shreenapyakurel@gmail.com')
+```
+
+if there is an error saying python environment is not available or something related run these 2 lines and repeat previous 2 lines
+```{r}
+ee_clean_pyenv()
+ ee_clean_credentials()
+```
+
+
+Visualize a Landsat image collection in a true color composite 
+```{r}
+image <- ee$Image("LANDSAT/LC08/C01/T1/LC08_044034_20140318")
+            
+Map$centerObject(image)
+
+vizParams <- list(
+  bands = c("B5", "B4", "B3"),
+  min = 5000, max = 15000, gamma = 1.3
+)
+
+Map$addLayer(image, vizParams, "Landsat 8 True color Composite")
+
+```
